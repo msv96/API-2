ff9b74378dae67144b9a0652a9815c8594e6020a
Srinivasan M <84406070+msv96@users.noreply.github.com>
2021-07-29 20:05:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 async function getData(word) {
     try {
-        const main = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`, {
-            method: "GET"
-        });
+        const main = await fetch(
+            `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`,
+            {
+                method: "GET",
+            }
+        );
         const data = await main.json();
-        const container = document.getElementById('container');
+        const container = document.getElementById("container");
         let ele = data[0];
-        let mainDiv = document.createElement('div');
-        mainDiv.classList.add('contentOne');
+        let mainDiv = document.createElement("div");
+        mainDiv.classList.add("contentOne");
         mainDiv.innerHTML =
             `
             <div class="mainDiv">
-            <p>Searched word : ${ele.word}</p>
-            <p>Text : ${ele.phonetics[0].text}</p>
+            <div>Searched word</div>
+            <div>${ele.word}</div>
+            <div>Text</div>
+            <div>${ele.phonetics[0].text}</div>
             <audio src="${ele.phonetics[0].audio}" controls></audio>
             </div>
             `;
         container.append(mainDiv);
         const el = data[0].meanings;
-        el.forEach((elem) => {
-            let anotherDiv = document.createElement('div');
-            anotherDiv.classList.add('contentTwo');
+        el.forEach((el) => {
+            let anotherDiv = document.createElement("div");
+            anotherDiv.classList.add("contentTwo");
             anotherDiv.innerHTML =
                 `
-                <div class="secDiv">
-                <p>Part of Speech : ${elem.partOfSpeech}</p>
-                <p>Definition : ${elem.definitions[0].definition}</p>
-                <p>Example : ${elem.definitions[0].example}</p>
+                <div class="anotherDiv">
+                <div>Part of Speech</div>
+                <div>${el.partOfSpeech}</div>
+                <div>Definition</div>
+                <div>${el.definitions[0].definition}</div>
+                <div>Example</div>
+                <div>${el.definitions[0].example}</div>
                 </div>
                 `;
             container.append(anotherDiv);
@@ -36,9 +44,13 @@ async function getData(word) {
         maindiv.innerText = err;
     }
 }
-
 function search() {
-    const word1 = document.getElementById('word').value;
-    getData(word1);
-    container.innerHTML = "";
+    const word = document.getElementById("word").value;
+    if (word == "") {
+        alert("Enter a word to Search");
+    }
+    else {
+        getData(word);
+        container.innerHTML = "";
+    }
 }
